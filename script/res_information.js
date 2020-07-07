46b13afaa444e9077411a9cfa6295ef74e994f4d
Loxymoxy <49946004+Loxymoxy@users.noreply.github.com>
2020-07-07 14:13:09 +0700

    setvalue result-js to res_information-js

---
+++ script/res_information.js
@@ -0,0 +1,373 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBca4gJadvrVWUy-q1RjE2LfMSjB8AVc4Y",
+    authDomain: "testdb-fc761.firebaseapp.com",
+    databaseURL: "https://testdb-fc761.firebaseio.com",
+    projectId: "testdb-fc761",
+    storageBucket: "testdb-fc761.appspot.com",
+    messagingSenderId: "505121024345",
+    appId: "1:505121024345:web:cd8aa4a23feb6eb031c448",
+    measurementId: "G-0GM7T5XXPM"
+
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+
+function setType(setID, setType) {
+    var del = false;
+
+    type.forEach((value, key) => {
+        console.log(value[0])
+        console.log(value[0])
+        if (setID == value[0]) {
+            type.splice(key, 1);
+            del = true
+        }
+    })
+    typeName.forEach((value, key) => {
+        if (setType == value[0]) {
+            typeName.splice(key, 1); z
+            del = true
+        }
+    })
+    if (!del) {
+        type.push([setID])
+        typeName.push([setType])
+    }
+}
+//test marker
+var markers = [
+
+    {
+        "title": 'เจ้รัต',
+        "lat": '7.8836764',
+        "lng": '98.3879528',
+        "description": ''
+    }
+    ,
+    {
+        "title": 'ขาหมู',
+        "lat": '7.890030',
+        "lng": '98.398178',
+        "description": ''
+    },
+    {
+        "title": 'บุญมาปอเปี๊ยะทอด',
+        "lat": '7.8838546',
+        "lng": '98.3132945',
+        "description": ''
+    }
+    ,
+    {
+        "title": 'ครัวยายด้วง',
+        "lat": '8.0597514',
+        "lng": '98.3417014',
+        "description": ''
+    }
+    , {
+        "title": 'เจ๊หน่อยขนมเบื้อง',
+        "lat": '7.8863897',
+        "lng": '98.3216157',
+        "description": ''
+    }
+    , {
+        "title": 'ทอดมันทำเอง',
+        "lat": '8.2680701',
+        "lng": '98.2290773',
+        "description": ''
+    }
+    , {
+        "title": 'วันจันทร์',
+        "lat": '7.885787',
+        "lng": '98.390745',
+        "description": ''
+    }
+    , {
+        "title": 'แหลมหินซีฟู้ด',
+        "lat": '7.8836764',
+        "lng": '98.3879528',
+        "description": ''
+    }
+    , {
+        "title": 'ภูเก็ต-ระย้า',
+        "lat": '7.8858435',
+        "lng": '98.3909914',
+        "description": ''
+    },
+];
+var getChaiName = localStorage.getItem("chaiName")
+var getEngName = localStorage.getItem("engName")
+var getThaiName = localStorage.getItem("thaiName")
+var getid = localStorage.getItem("setID");
+var getIm = localStorage.getItem("setImage");
+console.log("อะไรก็ได้")
+console.log(getid)
+console.log(getIm)
+
+function initMap() {
+    var location = { lat: 7.8948671, lng: 98.3531003 };
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: location
+        // center: new google.maps.LatLng(markers[0].lat, markers[0].lng)
+    });
+
+    var lat_lng = new Array();
+
+    for (i = 0; i < markers.length; i++) {
+        var data = markers[i]
+        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+        console.log(myLatlng)
+        //   lat_lng.push(myLatlng);
+
+
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+        });
+        // latlngbounds.extend(marker.position);
+    }
+
+    // var marker = new google.maps.Marker({
+    //   position: location,
+    //   map: map
+    // });
+    // var marker = new google.maps.Marker({
+    //   position: locate0,
+    //   map: map
+    // });
+    // var marker = new google.maps.Marker({
+    //   position: locate1,
+    //   map: map
+    // });
+
+}
+
+
+
+function tclick(valid) {
+
+    for (j = 0; j < markers.length; j++) {
+        var latlng = markers[j]
+        var myLatlng = new google.maps.LatLng(latlng.lat, latlng.lng);
+        // console.log(myLatlng)
+        // console.log(valid)
+        console.log(latlng)
+
+        if (valid == latlng.title) {
+            localStorage.setItem("valiable1", myLatlng)
+            localStorage.setItem("lat", latlng.lat)
+            localStorage.setItem("lng", latlng.lng)
+            localStorage.setItem("name", latlng.title)
+        }
+
+
+
+    }
+    // var getR = localStorage.getItem("valiable1")
+    var getR1 = localStorage.getItem("lat")
+    var getR2 = localStorage.getItem("lng")
+    // console.log(getR)
+    console.log(getR1)
+    console.log(getR2)
+
+    window.location.href = 'gps.html';
+
+}
+
+
+var getID;
+countSize.snapsize = 0;
+
+
+function countSize(snap) {
+    countSize.snapsize = snap
+}
+// function getSize(){
+//     return snapsize
+// }
+
+$(document).ready(function () {
+
+
+
+    db.collection("collection").get().then(function (snapshot) {
+        snapshot.forEach(function (doc) {
+
+            getCollec = doc.data().collection_name;
+            getid = doc.id
+            console.log(getid)
+
+            var nameCollec = `<div class="pointer" onclick="addMenu('${getid}')" ><p >${getCollec}</p></div>`
+            $("#showName").append(nameCollec);
+            console.log(getCollec)
+        });
+
+        console.log(snapshot.size)
+    });
+    $("#Cpop").unbind("click").click(function () {
+        // var getname = localStorage.getItem("setName")
+        // document.getElementById("showName").innerHTML = getname;
+        // console.log(getname);
+
+
+        $("#popup").modal();
+        // localStorage.clear(); 
+    });
+
+
+    $("#createCall").click(function () {
+        document.getElementById("createCollecton").value = '';
+        // $("#popup").modal('hide');
+
+
+        $("#submit").click(function () {
+
+            if (document.getElementById("createCollecton").value != "") {
+                db.collection("collection").get().then(function (snapshot) {
+
+                    snapshot.forEach(function (doc) {
+                        console.log(doc.id)
+                        getID = doc.id;
+                        snapsize = (snapshot.size + 1);
+
+                        countSize(snapsize)
+
+
+                    });
+
+                    db.collection("collection").add({
+                        collection_name: document.getElementById("createCollecton").value,
+                        id_nemu: [],
+                        id_collection: "c" + countSize.snapsize
+
+                    })
+
+                });
+
+
+            } else {
+                alert("Plase enter Collection Name")
+                console.log("มึงไม่ได้เขียน" + document.getElementById("createCollecton").value)
+
+
+            }
+
+            //  if(id_collection != id_collection)
+
+        });
+        $("#popCreate").modal();
+    });
+    $("#closeColl").click(function () {
+        localStorage.clear();
+
+    });
+
+var shaowName = `<div style="width:100%; margin-top: 21%;margin-left:0px; margin-right:0;">
+<div class="card text-light " style="width:400px; height:531px;background-color:rgb(128,0,128);">
+  <div style="margin-bottom: 5%; margin-top: 4%; font-size: 30px;">
+  ${getChaiName}
+  </div>
+  <div style="margin-bottom: 5%; font-size: 30px;">${getEngName}</div>
+  <div style="margin-bottom: 5%; font-size: 30px">${getThaiName}</div>
+</div>
+
+</div>`
+$("#Name_menu").append(shaowName);
+
+
+
+var showImage = `<div class="carousel-inner">  
+<div class="carousel-item active " style="margin-left:0px;">
+  
+  <img  src="${getIm}"
+    alt="Card image" style="width:100%; height:531px">
+</div>`
+$("#imageS").append(showImage)
+
+
+});
+
+
+
+function addMenu(getCollec) {
+    var arrayTest = []
+    var getvalue = getCollec
+    console.log(getvalue)
+
+    db.collection("collection").get().then(function (snapshot) {
+
+        snapshot.forEach(function (doc) {
+            var checkID = doc.data().id_nemu
+
+
+
+            if (doc.id == getvalue) {
+
+                for (i = 0; i < checkID.length; i++) {
+                    console.log("i" + i)
+                    console.log("ค่าarray " + doc.data().id_nemu[i])
+                    var dataa  = doc.data().id_nemu[i]
+
+                    arrayTest.push(dataa)
+
+                }
+
+
+            }
+            // db.collection("collection").doc(getvalue).update({
+
+
+            //     id_nemu: ["array2"]
+
+            // });
+        });
+
+       
+    }).then(function(){
+        console.log(arrayTest.length)
+        setvalue(getvalue,arrayTest)
+    })
+    console.log("ASDASD")
+    // console.log(doc.data().collection_name)
+    console.log(arrayTest.length)
+ 
+
+}
+
+function setvalue(idCollec , arrayName){
+
+    var getarrayName = arrayName
+    var check = false;
+    console.log(getarrayName)
+    console.log(arrayName.length)
+
+    // array.forEach(element => {
+    var getid = localStorage.getItem("setID");    
+    // });
+for (let index = 0; index < getarrayName.length; index++) {
+
+    console.log(getarrayName[index])
+
+    if ( getarrayName[index] == getid) {
+        check = true;
+      break;
+    }
+    
+}
+if(check == false){
+
+    getarrayName.push(getid)
+    console.log(getarrayName)
+}
+
+  
+    db.collection("collection").doc(idCollec).set({
+
+        id_nemu: getarrayName
+
+    }, { merge: true });
+
+}
--- script/result.js
+++ script/result.js
@@ -1,8 +1,8 @@
 var menu = [];
 var type = [];
 var meat = [];
-var text ;
-$(document).ready(function(){
+var text;
+$(document).ready(function () {
     menu = JSON.parse(localStorage.getItem("menu"));
     type = JSON.parse(localStorage.getItem("type"));
     meat = JSON.parse(localStorage.getItem("meat"));
@@ -12,45 +12,45 @@ $(document).ready(function(){
     console.log(meat)
     console.log(text)
 
-    if(text!==""){
+    if (text !== "") {
         $("#text").append(text);
-    }else{
+    } else {
         $("#text").hide();
         $("#head").hide();
-        $("#by").css("margin-top","20px")
+        $("#by").css("margin-top", "20px")
     }
     $("#rec").hide(text);
 
     $("#by").empty().show().append("Filter >").css("color", "rgb(109, 108, 108)");
-    if(type.length!==0){
-        type.forEach((item,index)=>{
-            
-            $("#by").append("<b>&nbsp;&nbsp;&nbsp;"+item[0]+"<b>");
+    if (type.length !== 0) {
+        type.forEach((item, index) => {
+
+            $("#by").append("<b>&nbsp;&nbsp;&nbsp;" + item[0] + "<b>");
         })
-    }if(meat.length!==0){
-        meat.forEach((item,index)=>{
-            
-            $("#by").append("<b>&nbsp;&nbsp;&nbsp;"+item[0]+"<b>");
+    } if (meat.length !== 0) {
+        meat.forEach((item, index) => {
+
+            $("#by").append("<b>&nbsp;&nbsp;&nbsp;" + item[0] + "<b>");
         })
-    }if(meat.length==0&&type.length==0){
+    } if (meat.length == 0 && type.length == 0) {
         $("#by").empty().show().append("Filter > ").css("color", "rgb(233, 233, 233)");
     }
 
-    menu.forEach((item , index) => {
-        
+    menu.forEach((item, index) => {
+
     });
-    
-    if(menu.length==0){
+
+    if (menu.length == 0) {
         var object = `<div class="col-12" style="margin-top: 5%; font-size: 50px; "> --- Not found --- </div>`
         $("#cardResult").append(object);
-    }else{
-        menu.forEach((item,value) => {
-            var spicy="";
-            if(item[6]=="no spicy"){
-                spicy=`<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; background-color: white;">
+    } else {
+        menu.forEach((item, value) => {
+            var spicy = "";
+            if (item[6] == "no spicy") {
+                spicy = `<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; background-color: white;">
                 </div>`
-            }else if(item[6]=="little spicy"){
-                spicy=`<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; 
+            } else if (item[6] == "little spicy") {
+                spicy = `<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; 
                 background-color: yellow;
                 background-image: url('img/spicy.png');
                 background-repeat: no-repeat;
@@ -61,8 +61,8 @@ $(document).ready(function(){
                 position: relative;
                 display: inline-block;">
                 </div>`
-            }else{
-                spicy=`<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; 
+            } else {
+                spicy = `<div class="col-2 spicy" id="spicy" style="border-radius: 15px 15px 15px 15px; 
                 background-color: 	#8B0000;
                 background-image: url('img/spicy.png');
                 background-repeat: no-repeat;
@@ -74,8 +74,8 @@ $(document).ready(function(){
                 display: inline-block;">
                 </div>`
             }
-            
-            var object = `<div class="col col-3" style="margin-top:2%;" onClick="createModal('${item[0]}','${item[1]}','${item[2]}','${item[3]}','${item[4]}','${item[5]}','${item[7]}')" data-toggle="modal" data-target="#showModal">
+
+            var object = `<div class="col col-3" style="margin-top:2%;" onClick="createModal('${item[0]}','${item[1]}','${item[2]}','${item[3]}','${item[4]}','${item[5]}','${item[7]}','${item[8]}')" data-toggle="modal" data-target="#showModal">
             <div class="card col-10" style=" margin:auto">
                 <div class="card-body">
                     <div class="row">
@@ -93,95 +93,104 @@ $(document).ready(function(){
 
 
 
-        $("#cardResult").append(object);
-        
-        
-    
-        
+            $("#cardResult").append(object);
+
+
+
+
         });
 
-    
-    }  
+
+    }
 })
 
-function createModal(name,name_2,name_3,meat,type,rec,pic){
+function createModal(name, name_2, name_3, meat, type, rec, pic, id) {
     $("#exampleModalLabel").empty();
     $("#exampleModalLabel").append(name);
-    
-    if(rec=="No"){
+
+    if (rec == "No") {
         $("#exampleModalLabel").css("margin-left", "0px;");
         $("#rex").hide();
-    }else{
+    } else {
         $("#rex").show();
     }
 
-    if(name_2!==""){
-        
+    if (name_2 !== "") {
+
         $("#name2").empty();
         $("#name2").append(name_2);
-    }else{
+    } else {
         $("#name2").hide();
         $("#slash").hide();
     }
-    if(name_3!==""){
+    if (name_3 !== "") {
         $("#name3").empty();
         $("#name3").append(name_3);
-    }else{
+    } else {
         $("#slash").hide();
         $("#name3").hide();
     }
 
-    if(type!=="t_012"){
+    if (type !== "t_012") {
         $("#type").empty();
-        if(type=="t_001"){
+        if (type == "t_001") {
             $("#type").append("Soup");
-        }else if(type=="t_002"){
+        } else if (type == "t_002") {
             $("#type").append("Noodle");
-        }else if(type=="t_003"){
+        } else if (type == "t_003") {
             $("#type").append("Cold dishes");
-        }else if(type=="t_004"){
+        } else if (type == "t_004") {
             $("#type").append("Rice");
-        }else if(type=="t_005"){
+        } else if (type == "t_005") {
             $("#type").append("Curry");
-        }else if(type=="t_006"){
+        } else if (type == "t_006") {
             $("#type").append("Barbecue");
-        }else if(type=="t_007"){
+        } else if (type == "t_007") {
             $("#type").append("Stir-fry");
-        }else if(type=="t_008"){
+        } else if (type == "t_008") {
             $("#type").append("Fried");
-        }else if(type=="t_009"){
+        } else if (type == "t_009") {
             $("#type").append("Dessert and snack");
-        }else if(type=="t_010"){
+        } else if (type == "t_010") {
             $("#type").append("Beverage");
-        }else if(type=="t_011"){
+        } else if (type == "t_011") {
             $("#type").append("Fruit");
         }
-    }else if(type=="t_012"){
+    } else if (type == "t_012") {
         $("#type").empty();
         $("#type").append("Other types");
     }
     $("#meatModal").show();
-    if(meat=="m_001"){
-        $("#meatModal").css("background-image","url('img/pig.png')")
-    }else if(meat=="m_002"){
-        $("#meatModal").css("background-image","url('img/chicken.png')")
-    }else if(meat=="m_003"){
-        $("#meatModal").css("background-image","url('img/cow.png')")
-    }else if(meat=="m_004"){
-        $("#meatModal").css("background-image","url('img/fish.png')")
-    }else if(meat=="m_005"){
-        $("#meatModal").css("background-image","url('img/shrimp.png')")
-    }else if(meat=="m_006"){
-        $("#meatModal").css("background-image","url('img/crab.png')")
-    }else if(meat=="m_007"){
-        $("#meatModal").css("background-image","url('img/shellfish.png')")
-    }else if(meat=="m_008"){
-        $("#meatModal").css("background-image","url('img/squid.png')")
-    }else{
+    if (meat == "m_001") {
+        $("#meatModal").css("background-image", "url('img/pig.png')")
+    } else if (meat == "m_002") {
+        $("#meatModal").css("background-image", "url('img/chicken.png')")
+    } else if (meat == "m_003") {
+        $("#meatModal").css("background-image", "url('img/cow.png')")
+    } else if (meat == "m_004") {
+        $("#meatModal").css("background-image", "url('img/fish.png')")
+    } else if (meat == "m_005") {
+        $("#meatModal").css("background-image", "url('img/shrimp.png')")
+    } else if (meat == "m_006") {
+        $("#meatModal").css("background-image", "url('img/crab.png')")
+    } else if (meat == "m_007") {
+        $("#meatModal").css("background-image", "url('img/shellfish.png')")
+    } else if (meat == "m_008") {
+        $("#meatModal").css("background-image", "url('img/squid.png')")
+    } else {
         $("#meatModal").hide();
     }
-    image=pic
-    console.log(image)
-    $("#imgModal").css("background-image", 'url('+image+')');
+    // image=pic
+    // console.log(image)
+    $("#imgModal").css("background-image", 'url(' + pic + ')').addClass(id);
+
+    $('.' + id).click(function () {
+        localStorage.setItem("thaiName", name_3);
+        localStorage.setItem("engName", name_2);
+        localStorage.setItem("chaiName", name);
+        localStorage.setItem("setImage", pic);
+        localStorage.setItem("setID", id);
+        window.location.href = 'res_information.html';
+    });
 
 }
