e044610fa7288bbb0e010f3662125445f6052e3d
Loxymoxy <49946004+Loxymoxy@users.noreply.github.com>
2020-07-15 21:32:46 +0700

    allcollection

---
+++ script/allCollection.js
@@ -0,0 +1,125 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDV6YCH-knnBtoCdvT1Z2wEkOU9y_NF_IU",
+  authDomain: "phuketfood-a4623.firebaseapp.com",
+  databaseURL: "https://phuketfood-a4623.firebaseio.com",
+  projectId: "phuketfood-a4623",
+  storageBucket: "phuketfood-a4623.appspot.com",
+  messagingSenderId: "590945091329",
+  appId: "1:590945091329:web:b0fdf7026b087cb8cc6500",
+  measurementId: "G-2MDJF4T26J"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
+var remove = firebase.firestore.FieldValue;
+var ele = 0;
+var arraySetdata = []
+$(document).ready(function () {
+  var showli = 0;
+  var refdb = db.collection("collection")
+  refdb.where("id_user", "==", "u_001").get().then((snapshot) => {
+
+    snapshot.forEach(doc => {
+      var collec = doc.data().collection_name;
+      var collecid = doc.data().id_menu;
+      var idmenu = doc.data().id_collection;
+      var idcoll = doc.id;
+      var data = doc.data()
+
+      arraySetdata.push(data)
+
+      // console.log(arraySetdata);
+      // id='UL${idmenu}' คือการอิงให้รู้ว่าจะเอาข้อความมาวางไว้ตรงช่องไหน
+      var thisx = this;
+      var showCname = `<li class="list-group-item" id="${idcoll}">${collec}<a type="button" class="btn fa- pull-right" onClick="deletecollecton('${idcoll}')">ลบ</a><a class="fa fa-print pull-right btn" onclick="selectCollection('${idcoll}')"></a><button onclick="dropup(this)" class="fa fa-caret-down pull-right btn" aria-hidden="false" data-toggle="collapse" data-target='#${idmenu}'></button>
+      <div class="collapse" id='${idmenu}'>
+      
+            <ul class="list-group list-group-flush " id='UL${idmenu}'></ul>
+            </div>
+            </li>
+            <hr>`
+      $('#showCname').append(showCname)
+      if (collecid == "") {
+        $("#UL" + idmenu).append("<center>empty</center>");
+      } else {
+        
+        arrayget(collecid, idcoll, idmenu);
+        // console.log(collecid);
+      }
+
+    });
+    
+  });
+
+
+
+});
+
+function dropup(x){
+  console.log()
+  console.log("12312121");
+  x.classList.toggle("fa-caret-up");
+}
+function arrayget(idnemu, idcoll, idmenu) {
+  // console.log(aarray);
+  for (let index = 0; index < idnemu.length; index++) {
+    const element = idnemu[index];
+    console.log(element);
+    db.collection("menu").where("id_Menu", "==", element).get().then((snapshot) => {
+      snapshot.forEach(doc => {
+        console.log(doc.data().chinese_Name);
+        var name = doc.data().english_Name;
+
+        var showli = `
+        <li class="list-group-item" style="margin-top:10px;" id="${element}" >${name} <a type="button" class="btn fa fa-minus pull-right" onClick="deleteArray('${idcoll}','${element}')"></a></li>`
+
+        $('#UL' + idmenu).append(showli)
+
+      });
+    });
+  }
+
+}
+// function delete array index 
+function deleteArray(idcall, array) {
+  db.collection("collection").doc(idcall).update({
+    id_menu: remove.arrayRemove(array)
+  });
+  $("#" + array).empty();
+}
+
+// function confirm delete collection
+function deletecollecton(idcollection) {
+  var r = confirm("Are you Sure For Delete Collection");
+  if (r == true) {
+    db.collection("collection").doc(idcollection).delete().then(function() {
+      console.log("Document successfully deleted!");
+  });
+  $("#"+ idcollection).empty();
+  } else {
+  console.log("cancle")
+  }
+  
+}
+
+// function set id ของ Collcetion ที่เลือก
+function selectCollection(collecdata){
+  console.log(collecdata);
+if(collecdata==""){
+  console.log("null");
+}else{
+  console.log(collecdata);
+  localStorage.setItem("datacollec",collecdata);
+}
+}
+
+var getdataaa = localStorage.getItem("datacollec")
+console.log(getdataaa);
+
+
+
+
+
+
